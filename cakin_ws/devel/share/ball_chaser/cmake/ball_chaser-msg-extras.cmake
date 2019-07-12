33510356bf56a55c0d093b96d9664dbd72d6b2a4
choi <ww1016y@gmail.com>
2019-07-12 11:16:35 +0900

    third commit

--- cakin_ws/devel/share/ball_chaser/cmake/ball_chaser-msg-extras.cmake
+++ cakin_ws/devel/share/ball_chaser/cmake/ball_chaser-msg-extras.cmake
@@ -1,2 +1,2 @@
 set(ball_chaser_MESSAGE_FILES "")
-set(ball_chaser_SERVICE_FILES "/home/nvidia/project2/cakin_ws/src/ball_chaser/srv/DriveToTarget.srv")
+set(ball_chaser_SERVICE_FILES "/home/nvidia/project2/project2/cakin_ws/src/ball_chaser/srv/DriveToTarget.srv")
