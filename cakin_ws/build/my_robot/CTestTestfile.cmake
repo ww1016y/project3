c1baa8b5ac5a03740fc2a07a8a2c2ce20fb367e5
ww1016y <ww1016y@gmail.com>
2019-07-23 19:41:55 +0900

    first

--- cakin_ws/build/my_robot/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jeongae13choi/project3/cakin_ws/src/my_robot
-# Build directory: /home/jeongae13choi/project3/cakin_ws/build/my_robot
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
